bc0f2faa916c585ed563e4da388b074de7551ae3
Ash Hill <ashfordhill@gmail.com>
2026-05-06 20:02:01 -0500

    rm src code. deprecated. latest lives in private repo

--- worker/src/zpl.ts
+++
@@ -1,7 +0,0 @@
-const LABEL_WIDTH_DOTS = 609;
-const QR_SIZE = 8;
-
-export function generateZpl(url: string): string {
-  const xPos = Math.floor((LABEL_WIDTH_DOTS - QR_SIZE * 25) / 2);
-  return `^XA\n^FO${xPos},50\n^BQN,2,${QR_SIZE}\n^FDQA,${url}^FS\n^XZ`;
-}
